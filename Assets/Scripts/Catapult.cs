e6cebc130019e62e5f80063861c8f735bf9b31bd
Clemaah <66416477+Clemaah@users.noreply.github.com>
2024-01-04 00:55:44 +0100

    Equation correction and some tweaking

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,73 +11,97 @@ public enum CatapultState
 
 public class Catapult : MonoBehaviour
 {
-    [SerializeField] private GameObject _SpawnProjectileLocation;
-    [SerializeField] private GameObject _Projectile;
-    [SerializeField] private GameObject _Arm;
+    [SerializeField] private GameObject _spawnProjectileLocation;
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private GameObject _arm;
+    [SerializeField] private GameObject _reloadMechanism;
 
-    [SerializeField] private float _minArmAngle = 6;
-    [SerializeField] private float _maxArmAngle = 77;
+    [SerializeField] private float _armLength = 6;
+    [SerializeField] private float _startAngle = 6;
+    [SerializeField] private float _endAngle = 77;
 
-    [SerializeField] private float _throwingRotationSpeed = 250;
-    [SerializeField] private float _reloadingRotationSpeed = 50;
+    [SerializeField] private float _throwingTime = 0.1f;
+    [SerializeField] private float _reloadingTime = 2;
 
     private CatapultState _state = CatapultState.Unarmed;
     private Projectile _currentProjectile = null;
+    private float _currentAngle = 0;
+
+    private float _throwingAngle = 0;
+    private float _throwingSpeed = 0;
+    private float _throwingRotationSpeed = 0;
+    private float _reloadRotationSpeed = 0;
 
     // Start is called before the first frame update
     private void Start()
     {
+        _throwingAngle = _endAngle - _startAngle;
 
+        float throwingArcLength = Mathf.PI * _armLength * _throwingAngle / 180;
+
+        _throwingSpeed = throwingArcLength / _throwingTime;
+        _throwingRotationSpeed = _throwingAngle / _throwingTime;
+        _reloadRotationSpeed = -_throwingAngle / _reloadingTime;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (_currentProjectile != null && !_currentProjectile.IsThrown)
-            _currentProjectile.transform.SetPositionAndRotation(_SpawnProjectileLocation.transform.position, _SpawnProjectileLocation.transform.rotation);
-
         switch (_state)
         {
             case CatapultState.Throwing:
-                RotateArm(_throwingRotationSpeed);
+                Throw();
                 break;
             case CatapultState.Reloading:
-                RotateArm(-_reloadingRotationSpeed);
+                Reload();
                 break;
             case CatapultState.Unarmed:
-                TryReload();
+                Rearmed();
                 break;
         }
     }
 
+    private void LateUpdate()
+    {
+        if (_currentProjectile != null && !_currentProjectile.IsFlying)
+            _currentProjectile.transform.SetPositionAndRotation(_spawnProjectileLocation.transform.position, _spawnProjectileLocation.transform.rotation);
+    }
+
     private void RotateArm(float rotationSpeed)
     {
-        _Arm.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        _arm.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        _currentAngle = _arm.transform.eulerAngles.x;
+    }
 
-        float currentAngle = _Arm.transform.eulerAngles.x;
+    private void Throw()
+    {
+        RotateArm(_throwingRotationSpeed);
 
-        if (currentAngle <= _minArmAngle)
-            _state = CatapultState.Unarmed;
-
-        else if (currentAngle >= _maxArmAngle)
+        if (_currentAngle >= _endAngle)
         {
-            _currentProjectile.Throw(_throwingRotationSpeed);
+            _currentProjectile.Throw(_throwingSpeed, _throwingTime);
             _state = CatapultState.Reloading;
         }
+    }
+
+    private void Reload()
+    {
+        RotateArm(_reloadRotationSpeed);
+        _reloadMechanism.transform.Rotate(Vector3.up, 5 * _reloadRotationSpeed * Time.deltaTime);
+
+        if (_currentAngle <= _startAngle)
+            _state = CatapultState.Unarmed;
+    }
+
+    private void Rearmed()
+    {
+        _currentProjectile = Instantiate(_projectile, _spawnProjectileLocation.transform.position, _spawnProjectileLocation.transform.rotation).GetComponent<Projectile>();
+        _state = CatapultState.Armed;
     }
 
     public void TryThrow()
     {
         if (_state == CatapultState.Armed)
             _state = CatapultState.Throwing;
-    }
-
-    public void TryReload()
-    {
-        if (_state == CatapultState.Unarmed)
-        {
-            _currentProjectile = Instantiate(_Projectile, _SpawnProjectileLocation.transform.position, _SpawnProjectileLocation.transform.rotation).GetComponent<Projectile>();
-            _state = CatapultState.Armed;
-        }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,11 +7,9 @@ public class Projectile : MonoBehaviour
 {
     static public float Gravity = -9.81f;
 
-    public bool IsThrown { get; private set; } = false;
-
-    private float _angle;
-    private float _initialZVelocity;
-    private float _initialYVelocity;
+    public bool IsFlying { get; private set; } = false;
+    private Vector3 _direction;
+    private float _speed = 0;
     private float _elapsedTime = 0;
 
     // Start is called before the first frame update
@@ -23,19 +21,25 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (IsThrown)
+        if (IsFlying)
         {
             _elapsedTime += Time.deltaTime;
-            transform.Translate(0, (_initialYVelocity + (0.5f * Gravity * Mathf.Pow(_elapsedTime, 2))) * Time.deltaTime,
-                _initialZVelocity * _elapsedTime * Time.deltaTime);
+
+            transform.Translate(
+                _direction.x * _speed * Time.deltaTime,
+                (_direction.y * _speed + (0.5f * Gravity * Mathf.Pow(_elapsedTime, 2))) * Time.deltaTime,
+                _direction.z * _speed * Time.deltaTime,
+                Space.World);
+
+            IsFlying = transform.position.y > 0;
         }
     }
 
-    public void Throw(float initialVelocity)
+    public void Throw(float initialSpeed, float initialElapsedTime)
     {
-        IsThrown = true;
-        _initialZVelocity = initialVelocity * Mathf.Cos(_angle * Mathf.Deg2Rad);
-        _initialYVelocity = initialVelocity * Mathf.Sin(_angle * Mathf.Deg2Rad);
-        _angle = 45;
+        IsFlying = true;
+        _direction = transform.up;
+        _speed = initialSpeed;
+        _elapsedTime = initialElapsedTime;
     }
 }
